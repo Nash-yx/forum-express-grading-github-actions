6694857b653d7f6f80a878f627f6fc7086cb6f4b
Nash <nash.git1217@gmail.com>
2025-05-30 17:34:38 +0800

    flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,14 +6,28 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: async (req, res) => {
-    const hash = await bcrypt.hash(req.body.password, 10)
-    await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: hash
-    })
-    return res.redirect('/signin')
+  signUp: async (req, res, next) => {
+    try {
+      if (req.body.password !== req.body.passwordCheck) {
+        throw new Error('Passwords do not match!')
+      }
+      const user = await User.findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+      if (user) throw new Error('Email already exists!')
+      const hash = await bcrypt.hash(req.body.password, 10)
+      await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+      })
+      req.flash('success_messages', '成功註冊帳號!')
+      return res.redirect('/signin')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
