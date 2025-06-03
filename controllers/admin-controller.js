83b4db6bb26b52cff474dc05b4140a0f5511d563
Nash <nash.git1217@gmail.com>
2025-06-04 00:06:10 +0800

    feat: admin update restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -31,9 +31,37 @@ const adminController = {
   },
   getRestaurant: async (req, res, next) => {
     try {
-      const restaurant = await Restaurant.findByPk(req.params.id, { raw: true })
+      const restaurant = await Restaurant.findByPk(req.params.id, {
+        raw: true
+      })
       if (!restaurant) throw new Error("Restaurant didn't exist!")
       return res.render('admin/restaurant', { restaurant })
+    } catch (err) {
+      next(err)
+    }
+  },
+  editRestaurant: async (req, res, next) => {
+    try {
+      const restaurant = await Restaurant.findByPk(req.params.id, {
+        raw: true
+      })
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+      return res.render('admin/edit-restaurant', { restaurant })
+    } catch (err) {
+      next(err)
+    }
+  },
+  putRestaurant: async (req, res, next) => {
+    try {
+      const { name, tel, address, openingHours, description } = req.body
+      if (!name) throw new Error('Restaurant name is required!')
+
+      const restaurant = await Restaurant.findByPk(req.params.id)
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+      await restaurant.update({ name, tel, address, openingHours, description })
+      req.flash('success_messages', 'restaurant was successfully to updated')
+      return res.redirect('/admin/restaurants')
     } catch (err) {
       next(err)
     }
