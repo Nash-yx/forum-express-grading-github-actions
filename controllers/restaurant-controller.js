4263065e47c9875714297c543736545363f8e791
Nash <nash.git1217@gmail.com>
2025-06-09 23:08:26 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,5 +1,21 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
+  getRestaurants: async (req, res, next) => {
+    try {
+      const restaurants = await Restaurant.findAll({
+        include: Category,
+        nest: true,
+        raw: true
+      })
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      return res.render('restaurants', { restaurants: data })
+    } catch (err) {
+      next(err)
+    }
     return res.render('restaurants')
   }
 }
