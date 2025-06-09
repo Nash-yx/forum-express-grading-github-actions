bc1bcf0454cce7184f53537e2d85e045153b9183
Nash <nash.git1217@gmail.com>
2025-06-09 16:43:40 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -7,6 +7,17 @@ const categoryController = {
     } catch (err) {
       next(err)
     }
+  },
+  postCategories: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      if (!name) throw new Error('Category name is required!')
+
+      await Category.create({ name })
+      return res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
