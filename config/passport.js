3e44035fabc4194836994f2c28497bc5dc124a3a
Nash-yx <nash.git1217@gmail.com>
2025-06-15 17:06:35 +0800

    feat: add followship model

--- config/passport.js
+++ config/passport.js
@@ -28,7 +28,9 @@ passport.deserializeUser(async (id, done) => {
     const user = await User.findByPk(id, {
       include: [
         { model: Restaurant, as: 'FavoritedRestaurants' }, // or 'FavoritedRestaurants'
-        { model: Restaurant, as: 'LikedRestaurants' }
+        { model: Restaurant, as: 'LikedRestaurants' },
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
       ]
     })
     // console.log(user.toJSON())
