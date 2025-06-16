4e6de7e0e60cbaafa4fb9adbdfe0cf4c0982d00d
Nash <nash.git1217@gmail.com>
2025-06-16 16:39:30 +0800

    feat: add getRestaurants api

---
+++ controllers/apis/restaurant-controller.js
@@ -0,0 +1,48 @@
+const { Restaurant, Category } = require('../../models')
+const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+
+const restaurantController = {
+  getRestaurants: async (req, res, next) => {
+    try {
+      const DEFAULT_LIMIT = 9
+      const categoryId = Number(req.query.categoryId) || ''
+
+      const page = Number(req.query.page) || 1
+      const limit = Number(req.query.limit) || DEFAULT_LIMIT
+      const offset = getOffset(limit, page)
+
+      const [restaurants, categories] = await Promise.all([
+        Restaurant.findAndCountAll({
+          include: Category,
+          where: {
+            ...(categoryId ? { categoryId } : {})
+          },
+          limit,
+          offset,
+          nest: true,
+          raw: true
+        }),
+        Category.findAll({ raw: true })
+      ])
+      const favoritedRestaurantsId = req.user?.FavoritedRestaurants ? req.user.FavoritedRestaurants.map(fr => fr.id) : []
+      // req.user?.FavoritedRestaurants 等價 req.user && req.user.FavoritedRestaurants
+      const likedRestaurantsId = req.user?.LikedRestaurants ? req.user.LikedRestaurants.map(like => like.id) : []
+      const data = restaurants.rows.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50),
+        isFavorited: favoritedRestaurantsId.includes(r.id),
+        isLiked: likedRestaurantsId.includes(r.id)
+      }))
+      return res.json({
+        restaurants: data,
+        categories,
+        categoryId,
+        pagination: getPagination(limit, page, restaurants.count)
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = restaurantController
